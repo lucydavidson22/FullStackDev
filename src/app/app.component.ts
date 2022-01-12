6f34e5efa2c2c8693f31ca4187ec17af03173942
Lucy Davidson <lucydavidson@Lucys-MacBook-Air.local>
2022-01-11 22:45:55 -0500

    the basics videos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',         //assigns string as value "app root"
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']     //this is an array because it can point to more than one css page
+  
 })
 export class AppComponent {
+  username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
